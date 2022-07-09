0a4d83081d83f6da7705f6e88f552a1c7b8417db
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-09 12:29:25 -0300

    Add initial implementation for csr multiplication and block based csr multiplication

---
+++ csr_product.cuh
@@ -0,0 +1,49 @@
+// Kernel that implements spmv product using CSR matrix
+template <typename data_type>
+__global__ void spmv_csr_kernel(const int *row_ptr, const int *col_ind, const data_type *val, const data_type *x, data_type *y, const int n) {
+  const int row = blockIdx.x * blockDim.x + threadIdx.x;
+  if (row < n) {
+    const int start = row_ptr[row];
+    const int end = row_ptr[row + 1];
+
+    data_type sum = 0;
+    for (int i = start; i < end; i++) {
+      const int col = col_ind[i];
+      sum += val[i] * x[col];
+    }
+
+    y[row] = sum;
+  }
+}
+
+// Kernel that implements spmv product using Block CSR matrix
+template <typename data_type>
+__global__ void bcsr_spmv_kernel_thread_per_row_row_major_matrix (
+  const int n_block_rows,
+  const int bs,
+  const int * __restrict__ col_ids,
+  const int * __restrict__ row_ptr,
+  const data_type * __restrict__ data,
+  const data_type * __restrict__ x,
+  data_type *y
+) {
+  const int idx = blockIdx.x * blockDim.x + threadIdx.x;
+  const int row = idx % bs;
+  const int block_row = idx / bs;
+  const int first_block = row_ptr[block_row];
+  const int last_block = row_ptr[block_row + 1];
+
+  if (row < bs && block_row < n_block_rows) {
+    data_type local_out = 0.0;
+
+    for (int block = first_block; block < last_block; block++) {
+      const int first_col = col_ids[block] * bs;
+
+      for (int col = 0; col < bs; col++) {
+        local_out += x[first_col + col] * data[block * bs * bs + row * bs + col];
+      }
+    }
+
+    y[block_row * bs + row] = local_out;
+  }
+}
