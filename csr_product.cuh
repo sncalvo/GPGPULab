c5cd2e6a0451e4fe4bd0ec01ee4313117c661fa1
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-19 19:48:16 -0300

    I dont know

--- csr_product.cuh
+++ csr_product.cuh
@@ -9,6 +9,8 @@ __global__ void spmv_csr_kernel(
 ) {
   const int row = blockIdx.x * blockDim.x + threadIdx.x;
 
+  printf("AH!!! %d: %f\n", row, A.colN);
+
   if (row >= A.colN) {
     return;
   }
