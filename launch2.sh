eb0979a2955bf3c5f5219321b0d899b7fbc423da
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-19 21:09:47 -0300

    I dont know

--- launch2.sh
+++ launch2.sh
@@ -11,7 +11,7 @@
 #SBATCH --gres=gpu:1
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=sncalvo5@gmail.com
-#SBATCH -o salida1.out
+#SBATCH -o salida2.out
 
 export PATH=$PATH:/usr/local/cuda/bin
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
@@ -19,3 +19,25 @@ export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
 nvcc ./main.cu -o solution2
 
 ./solution2 10 10
+
+echo '============================'
+echo 'NORMAL RUN STARTING'
+echo '============================'
+
+./solution2 10000 10000
+
+echo '============================'
+echo 'TESTING TIME'
+echo '============================'
+
+nvprof ./solution2 10000 10000
+
+echo '============================'
+echo 'TESTING EFFICIENCY'
+echo '============================'
+
+nvprof --metrics gld_efficiency,gst_efficiency,shared_efficiency ./solution2 10000 10000
+
+echo '============================'
+echo 'END'
+echo '============================'
