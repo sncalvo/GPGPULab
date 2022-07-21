144e0d096580982474f5d5ccf98da371816a292c
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-20 21:16:47 -0300

    I dont know

--- csr_product.cuh
+++ csr_product.cuh
@@ -71,10 +71,10 @@ __global__ void bsr_vector_kernel(
     // }
 
     // Multiply dense block by dense vector
-    for (int j = 0; j < 8; j++) {
-      for (int k = 0; k < 8; k++) {
-        result[idx + j] += block[j][k] * x[col + k];
-      }
-    }
+    // for (int j = 0; j < 8; j++) {
+    //   for (int k = 0; k < 8; k++) {
+    //     result[idx + j] += block[j][k] * x[col + k];
+    //   }
+    // }
   }
 }
