7f19825f39feb77f76fb96b71f61ea8f9b2aad3b
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-20 21:20:52 -0300

    I dont know

--- csr_product.cuh
+++ csr_product.cuh
@@ -74,7 +74,7 @@ __global__ void bsr_vector_kernel(
     for (int j = 0; j < 8; j++) {
       for (int k = 0; k < 8; k++) {
         // result[idx + j] += block[j][k] * x[col + k];
-        const int hola = block[j][k];
+        const int hola = x[col + k];
       }
     }
   }
