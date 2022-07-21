8a4e676e0f655895342c47bbe567a9424a23affd
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-20 21:10:41 -0300

    I dont know

--- csr_product.cuh
+++ csr_product.cuh
@@ -60,11 +60,10 @@ __global__ void bsr_vector_kernel(
     const int start = A.blStart[i];
     const int end = A.blStart[i + 1];
 
-    continue;
     for (int j = 0; j < 8; j++) {
       for (int k = 0; k < 8; k++) {
         if (bitMap & (1 << (j*k)) && start + j + k < end) {
-          block[j][k] = A.val[start + j + k];
+          // block[j][k] = A.val[start + j + k];
         } else {
           block[j][k] = 0;
         }
