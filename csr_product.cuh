0f6a3241183f7c26f330cf1b03561d232feb63f3
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-23 12:30:56 -0300

    Stuff

--- csr_product.cuh
+++ csr_product.cuh
@@ -183,5 +183,5 @@ __global__ void bsr_vector_kernel_3(
   //     block[7][0], block[7][1], block[7][2], block[7][3], block[7][4], block[7][5], block[7][6], block[7][7]);
   // }
 
-  atomicAdd(&result[idx * 8 + j], block[j][i] * x[col * 8 + i]);
+  atomicAdd(&result[idx * 8 + j], block[j][i] * x[col + i]);
 }
