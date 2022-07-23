5b8048bfb6aa46c9795c49b896b99d17f5778604
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-23 17:47:39 -0300

    Stuff

--- csr_product.cuh
+++ csr_product.cuh
@@ -123,7 +123,7 @@ __global__ void bsr_vector_kernel_3(
   // printf("%llu\n", bitMap);
   // printf("%llu --- %s,\n", bitMap & (0x8000000000000000 >> (j*8 + i)), bitMap & (0x8000000000000000 >> (j*8 + i)) ? "true" : "false");
   if (bitMap & (0x8000000000000000 >> (j*8 + i))) {
-    printf("Writing: %.2f, idx %d\n", A.val[start + numberOfVals], start + numberOfVals);
+    printf("Writing: %.2f, idx %d\n, col: %d, row: %d, start: %d, rowStart: %d", A.val[start + numberOfVals], start + numberOfVals, col, rowIdx, start, rowStart);
     block[j][i] = A.val[start + numberOfVals];
   } else {
     // block[j][i] = 0;
