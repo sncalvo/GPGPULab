f2f0e777accc6bdfa322ef14725c85ac9ebb5cd1
Santiago Calvo <48108808+sncalvo@users.noreply.github.com>
2022-07-23 17:36:19 -0300

    Stuff

--- csr_product.cuh
+++ csr_product.cuh
@@ -121,7 +121,7 @@ __global__ void bsr_vector_kernel_3(
   const int numberOfVals = __popcll(bitMap >> (64 - (j*8 + i)));
 
   // printf("%llu\n", bitMap);
-  printf("%llu --- %s,\n", bitMap & (0x8000000000000000 >> (j*8 + i)), bitMap & (0x8000000000000000 >> (j*8 + i)) != 0 ? "true" : "false");
+  printf("%llu --- %s,\n", bitMap & (0x8000000000000000 >> (j*8 + i)), bitMap & (0x8000000000000000 >> (j*8 + i)) ? "true" : "false");
   if (bitMap & (0x8000000000000000 >> (j*8 + i)) != 0) {
     printf("Writing: %.2f, with: start: %d, nValues: %d \n", A.val[start + numberOfVals], start, numberOfVals);
     block[j][i] = A.val[start + numberOfVals];
